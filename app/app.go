d67feb60c5b3ca841f53a67dc54600cca5f64833
Ondra Tkaczyszyn <ondtka@gmail.com>
2019-01-25 09:55:20 +0100

    Got authentication working (only with pre-existing users currently).

--- app/app.go
+++ app/app.go
@@ -4,19 +4,33 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"log"
+	"time"
 	"net/http"
 	"os"
+	"encoding/json"
+	"github.com/gomodule/redigo/redis" // cache api access token
+	"strconv" // convert number to string
+	"github.com/satori/go.uuid" // convert number to string
 )
 
+var cache redis.Conn
+const tokenExpireTime = 360
+
 func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "This is the RESTful api")
+	fmt.Fprintf(w, "application root")
+}
+func loginHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+
 }
 
 func main() {
+
+	initCache()
 	router := httprouter.New()
 	router.GET("/", indexHandler)
+	router.GET("/login",loginHandler);
+	router.GET("/",indexHandler);
 
-	// print env
 	env := os.Getenv("APP_ENV")
 	if env == "production" {
 		log.Println("Running api server in production mode")
@@ -24,5 +38,59 @@ func main() {
 		log.Println("Running api server in dev mode")
 	}
 
-	http.ListenAndServe(":8080", router)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func initCache(){
+	conn,err := redis.DialURL("redis://localhost")
+	if err != nil {
+		panic(err)
+	}
+	cache = conn
+}
+
+// TEMPORARY list of admissible users
+var users = map[string]string{
+	"ahojky": "hranolky",
+	"nazdar": "kvazar",
+}
+
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+func ValidatePass(received string,expected string) bool{
+	// TEMP: here for easy implementation of at least hypothetically secure password authentication
+	return (received == expected)
+}
+
+func Signin(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err!=nil{
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	expectedPassword, ok := users[creds.Username]
+
+	if !ok || !ValidatePass(expectedPassword,creds.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	// generate unique session token
+	sessionToken, uerr := uuid.NewV4()
+	_, err = cache.Do("SETEX", sessionToken.String(),strconv.Itoa( tokenExpireTime ), creds.Username)
+	if err != nil || uerr != nil{
+		log.Println("Error setting cache or generating uuid.")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    sessionToken.String(),
+		Expires: time.Now().Add(tokenExpireTime*time.Second),
+	})
 }
